fc0316af85f39f2bc8264161f07e7df83f35070e
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-04-28 02:03:42 +0200

    GULP-ed css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const sourceMaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const {src, series, parallel, dest, watch} = require("gulp");
 
-const {src,dest} = require("gulp");
-const minifyCss = require("gulp-clean-css");
+const cssPath = "./project/**/**/*.css";
 
-const bundleCss = () => {
-    return src("/")
+function bundleCss() {
+    return src(cssPath)
+        .pipe(sourceMaps.init())
+        .pipe(concat("bundle.css"))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourceMaps.write("."))
+        .pipe(dest("./project/project.css.bundle/"));
 }
+
+// function cssWatch() {
+//     watch([cssPath], {interval: 1000}, parallel(bundleCss));
+// }
+
+exports.bundleCss = bundleCss;
