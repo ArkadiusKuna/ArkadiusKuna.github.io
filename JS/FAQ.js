b67b475287c368cb2aa834a27c47cc09aa0a7e00
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-08 20:45:45 +0200

    FAQ SEARCH BAR rewritten to BEM, fix footerr display in max resolution, minor changes to blocks CSS properties to make the more reusable

--- JS/FAQ.js
+++ JS/FAQ.js
@@ -28,8 +28,8 @@ FAQquestions.forEach((question) => {
 
 function hideInput() {
   inputBar.classList.remove("active");
-  accordionTitle.classList.remove("hide-header");
-  searchBtn.style.right = "-1vw";
+  accordionTitle.classList.remove("hide-title");
+  searchBtn.style.right = "-25px";
   inputBox.classList.remove("add-border");
   closeBtn.style.visibility = "hidden";
   inputBar.value = "";
@@ -39,18 +39,17 @@ function hideInput() {
 function showInput() {
   if(inputBar.classList.contains("active")) {
     inputBar.classList.remove("active");
-    accordionTitle.classList.remove("hide-header");
-    searchBtn.style.right = "-1vw";
+    accordionTitle.classList.remove("hide-title");
+    searchBtn.style.right = "-25px";
     inputBox.classList.remove("add-border");
     closeBtn.style.visibility = "hidden";
     return;
   }
   inputBar.classList.add("active");
-  accordionTitle.classList.add("hide-header");
-  searchBtn.style.right = "59.8vw";
+  accordionTitle.classList.add("hide-title");
+  searchBtn.style.right = "850px";
   inputBox.classList.add("add-border");
   closeBtn.style.visibility = "visible";
-  inputBar.click();
 }
 
 function liveSearch() {
@@ -61,6 +60,7 @@ function liveSearch() {
       FAQquestions[i].classList.remove("hidden");
       FAQquestions[i].classList.add("accord-drop");
       notMatch.style.display = "none";
+      inputText.style.display = "inline"
     } else {
       FAQquestions[i].classList.add("hidden");
       notMatch.style.display = "none";
