622221377bda8fa751a2c9dff769fb587e0719e6
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-10 16:02:24 +0200

    add price slider JS

---
+++ JS/rangeSlider.js
@@ -0,0 +1,12 @@
+$("#price-slider").ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: "80",
+    max: "120",
+    step: "2",
+    min_interval: "2",
+    max_interval: "2",
+    hide_min_max: "true",
+    hide_from_to: "true",
+
+});
