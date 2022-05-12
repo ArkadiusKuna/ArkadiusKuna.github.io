e9bba447ccb67c473b6f791cfffc7e4a1528c676
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-12 11:54:49 +0200

    drevert changes for single css property to every class

--- JS/instafeed.js
+++ JS/instafeed.js
@@ -1,22 +1,22 @@
 var userFeed = new Instafeed({
-    get: 'kunarek',
-    target: "instafeed-slider",
-    resolution: 'standard_resolution',
-    template: '<div class="instafeed_slider__swiper_slide swiper-slide"><div class="instafeed__caption"><p class="instafeed__text">{{caption}}</p></div><img title="{{caption}}" class="instafeed__image" src="{{image}}" /></div>',
-    accessToken: 'IGQVJVZADQ2YnZAleFVfbGJIb0lLX2J0X1hvVkdUNUhSeF9WY2Y1ZAHFCNFVhUXhhX2NwV040UkZA5eEtEbmxMSF9zdl9RUm1udmg2NkVhX0tmRmdvSU1BMFFGUHNTRWNXMFJScUUxSE9mWHNEM2JydFFwSwZDZD',
-    sortBy: 'standard-recent',
-    after: function () {
-
-      const instafeedSwiper = new Swiper('.instafeed-slider', {
-        slidesPerView: 5,
-        slidesPerGroup: 3,
-        spaceBetween: 40,
-        allowTouchMove: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }
-      })
-  }
-})
+  get: "kunarek",
+  target: "instafeed-slider",
+  resolution: "standard_resolution",
+  template:
+    '<div class="instafeed_slider__swiper_slide swiper-slide"><img class="instafeed__image" src="{{image}}"</img><div class="instafeed__caption"><p class="instafeed__text">{{caption}}</p></div></div>',
+  accessToken:
+    "IGQVJVZADQ2YnZAleFVfbGJIb0lLX2J0X1hvVkdUNUhSeF9WY2Y1ZAHFCNFVhUXhhX2NwV040UkZA5eEtEbmxMSF9zdl9RUm1udmg2NkVhX0tmRmdvSU1BMFFGUHNTRWNXMFJScUUxSE9mWHNEM2JydFFwSwZDZD",
+  sortBy: "standard-recent",
+  after: function () {
+    const instafeedSwiper = new Swiper(".instafeed-slider", {
+      slidesPerView: 5,
+      slidesPerGroup: 3,
+      allowTouchMove: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  },
+});
 userFeed.run();
