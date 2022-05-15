9d05c77ef60308f4a3129db1219d41d3f21e89b7
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-16 00:14:03 +0200

    fix: range slider JS added and some style changes

--- JS/rangeSlider.js
+++ JS/rangeSlider.js
@@ -1,12 +1,34 @@
-$("#price-slider").ionRangeSlider({
-    skin: "round",
-    type: "double",
-    min: "80",
-    max: "120",
-    step: "2",
-    min_interval: "2",
-    max_interval: "2",
-    hide_min_max: "true",
-    hide_from_to: "true",
+let textFrom = document.getElementById("value-from");
+let textTo = document.getElementById("value-to");
 
+$("#price-slider").ionRangeSlider({
+  skin: "round",
+  type: "double",
+  min: "80",
+  max: "120",
+  step: "2",
+  hide_min_max: "true",
+  hide_from_to: "true",
+
+  onStart: function (data) {
+    let valueFrom = data.from;
+    let valueTo = data.to;
+
+    textFrom.innerText = valueFrom + ",00 zł";
+    textTo.innerText = valueTo + ",00 zł";
+  },
+  onChange: function (data) {
+    let valueFrom = data.from;
+    let valueTo = data.to;
+
+    textFrom.innerText = valueFrom + ",00 zł";
+    textTo.innerText = valueTo + ",00 zł";
+  },
+  onFinish: function (data) {
+    let valueFrom = data.from;
+    let valueTo = data.to;
+
+    textFrom.innerText = valueFrom + ",00 zł";
+    textTo.innerText = valueTo + ",00 zł";
+  },
 });
