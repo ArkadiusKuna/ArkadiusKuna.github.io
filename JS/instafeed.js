9c01d400ed4d1b3fe9facbef08b7a4c3a76dab8d
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-19 00:38:35 +0200

    fix instafeed navigation buttons

--- JS/instafeed.js
+++ JS/instafeed.js
@@ -3,7 +3,7 @@ var userFeed = new Instafeed({
   target: "instafeed-slider",
   resolution: "standard_resolution",
   template:
-    '<div class="instafeed_slider__swiper_slide swiper-slide"><img class="instafeed__image" src="{{image}}"</img><div class="instafeed__caption instafeed__caption--hovered"><p class="instafeed__text">{{caption}}</p></div></div>',
+    '<div class="instafeed-slider__swiper-slide swiper-slide"><img class="instafeed__image" src="{{image}}"</img><div class="instafeed__caption instafeed__caption--hovered"><p class="instafeed__text">{{caption}}</p></div></div>',
   accessToken:
     "IGQVJVZADQ2YnZAleFVfbGJIb0lLX2J0X1hvVkdUNUhSeF9WY2Y1ZAHFCNFVhUXhhX2NwV040UkZA5eEtEbmxMSF9zdl9RUm1udmg2NkVhX0tmRmdvSU1BMFFGUHNTRWNXMFJScUUxSE9mWHNEM2JydFFwSwZDZD",
   sortBy: "standard-recent",
