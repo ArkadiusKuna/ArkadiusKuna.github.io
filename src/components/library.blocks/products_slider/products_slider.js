836424c55adddb2b581f91a558e1aa9d20d8e3a6
Arkadiusz Kuna <kuna.arek@gmail.com>
2022-05-30 15:34:37 +0200

    initial production commit

--- src/components/library.blocks/products_slider/products_slider.js
+++
@@ -1,9 +0,0 @@
-const productsSwiper = new Swiper(".products-slider", {
-  slidesPerView: 4,
-  slidesPerGroup: 4,
-  allowTouchMove: false,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
